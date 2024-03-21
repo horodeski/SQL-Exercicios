6e54b45197db81a28edb2a9feeed1d5e2ab770dd
Geovana Horodeski <geovana.horodeski06@gmail.com>
2024-03-21 15:12:54 -0300

    Create Lista7.sql

---
+++ Lista7.sql
@@ -0,0 +1,13 @@
+--  Lista 7 - a) Excluindo todos os pedidos que forem menores que R$ 1.000,00, que não possuam itens cadastrados e que sejam no mês de junho/2003
+
+DELETE REQUEST FROM REQUEST
+JOIN PRODUCTREQUEST ON PRODUCTREQUEST.CDREQUEST = REQUEST.CDREQUEST
+WHERE REQUEST.VLTOTAL < 1000 AND YEAR(REQUEST.DTREQUEST) = 2003 AND MONTH(REQUEST.DTREQUEST) = 6 AND PRODUCTREQUEST.CDPRODUCT IS NULL
+
+-- Lista 7 - b) Atualizando o preço do produto como sendo a média dos valores unitários de venda, somente para produtos que venderam menos que 800 unidades
+
+UPDATE PRODUCT
+SET VLPRICE = AVG(PRODUCTREQUEST.VLUNITARY)
+FROM PRODUCT
+JOIN PRODUCTREQUEST ON PRODUCTREQUEST.CDPRODUCT = PRODUCT.CDPRODUCT
+WHERE PRODUCTREQUEST.QTAMOUNT < 800
